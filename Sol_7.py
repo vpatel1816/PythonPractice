bfbdc15fb217d48b0cc645601898dd26d4381aa1
vishall <65184972+vpatel1816@users.noreply.github.com>
2020-07-28 15:44:13 -0400

    Sol_7 Inital Commit

---
+++ Sol_7.py
@@ -0,0 +1,10 @@
+#7 Find second min and second largest value of a list and their indexes also
+
+int_list = [5,2,3,4]
+
+min = int_list[0]
+for i in int_list:
+	if i<min:
+		print("here")
+		min == i
+	print(min)
