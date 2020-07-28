253268fb273d42b5d0b204fbd99bf86d52339a0e
vishall <65184972+vpatel1816@users.noreply.github.com>
2020-07-28 16:03:40 -0400

    Able to work with Even list, but Odd needs to be work on

--- Sol_8.py
+++ Sol_8.py
@@ -1,7 +1,15 @@
 #8 wright a program to split a list into two sub list from the middle
 
-newlist = [1,2,3,4,5,6,7,8]
+newlist = [1,2,3,4,5,6,7]
 firstlist = []
+secondlist = []
+
+mid = int(len(newlist)/2)
+
 for i in range(int(len(newlist)/2)):
 	firstlist.append(newlist[i])
-print(firstlist)
+
+for i in range(mid):
+	secondlist.append(newlist[i+mid])
+
+print(firstlist, secondlist, mid)
