d75203cb5a89dddd90cc1b60932134fbddd135f3
vishall <65184972+vpatel1816@users.noreply.github.com>
2020-07-28 17:17:24 -0400

    Solution 7

--- Sol_7.py
+++ Sol_7.py
@@ -1,10 +1,17 @@
 #7 Find second min and second largest value of a list and their indexes also
 
-int_list = [5,2,3,4]
+int_list = [6,7,8,4,45,56,89,90,98,1999]
 
 min = int_list[0]
+second_min = int_list[0]
+second_high = int_list[0]
+
 for i in int_list:
-	if i<min:
-		print("here")
-		min == i
-	print(min)
+	if i < min:
+		second_min = min 
+		min = i
+	else:
+		second_high = max
+		max = i
+
+print("Min: ",min,",Second Min :", second_min,",Max: ", max,",Second Max: " ,second_high)
